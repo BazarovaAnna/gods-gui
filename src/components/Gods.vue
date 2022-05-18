2d4bba34fac005bfefadb6f7f1efc20c912053b4
annab <243845@niuitmo.ru>
2022-05-18 13:54:05 +0300

    buttons for gods design

--- src/components/Gods.vue
+++ src/components/Gods.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="gods">
     <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">Перевернуть сообщение</button>
+    <button class="heal" v-on:click="reverseMessage">Исцелить</button>
+    <button class="take" v-on:click="reverseMessage">З̶̯͒̚а̴̼̬̑͛б̷̛͙͒р̸͈͕͛̕а̶̧̪̽̓т̸̛̤͖́ь̶̺́͑ ̷̱͝д̶͖͋̊у̴̛̤ш̴̣̈́̌у̸͎͖̒ </button>
   </div>
 </template>
 
@@ -16,6 +17,7 @@ export default {
   },
   methods: {
     reverseMessage: function () {
+      //java method
       this.message = this.message.split('').reverse().join('')
     }
   }
@@ -23,5 +25,5 @@ export default {
 </script>
 
 <style scoped>
-
+  @import '../assets/styles/gods/index.css';
 </style>
