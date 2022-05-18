53648650d532884d871b9e50f3cffe87537e9e8b
annab <243845@niuitmo.ru>
2022-05-18 12:57:44 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Gods/>
+</template>
+
+<script>
+import Gods from './components/Gods.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Gods
+  }
+}
+</script>
+
+<style>
+  @import './assets/styles/app.css';
+</style>
---
+++ src/components/Gods.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="gods">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Перевернуть сообщение</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GodsVi",
+  el: '#gods',
+  data() {
+    return {
+      message: 'Привет, Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
