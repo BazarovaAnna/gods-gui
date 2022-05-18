4a9b8b55af232150d3ab8ad325e12435a9c0d5c7
annab <243845@niuitmo.ru>
2022-05-18 18:48:40 +0300

    buttons gods design

--- src/components/Gods.vue
+++ src/components/Gods.vue
@@ -1,24 +1,72 @@
 <template>
   <div id="gods">
-    <p>{{ message }}</p>
-    <button class="heal" v-on:click="reverseMessage">Исцелить</button>
-    <button class="take" v-on:click="reverseMessage">З̶̯͒̚а̴̼̬̑͛б̷̛͙͒р̸͈͕͛̕а̶̧̪̽̓т̸̛̤͖́ь̶̺́͑ ̷̱͝д̶͖͋̊у̴̛̤ш̴̣̈́̌у̸͎͖̒ </button>
+    <div class="content">
+      <div><img class="cursed-image" src="../assets/cursed.png" alt="cursed" v-show="cursed"/></div>
+      <table class="info">
+        <tr>
+        <td class="main-info">
+          <p>Имя: {{ nameP }}</p>
+          <p>Род деятельности: {{ job }}</p>
+          <p>Возраст: {{ age }}</p>
+          <p>Болезнь: {{ disease }}</p>
+        </td>
+        </tr>
+        <tr>
+        <td class="add-info" v-if="true">{{ addInfo }}</td>
+        </tr>
+      </table>
+      <div class="buttons-layer">
+        <button class="heal" v-on:click="heal">Исцелить</button>
+        <button class="take" v-on:click="death">З̶̯͒̚а̴̼̬̑͛б̷̛͙͒р̸͈͕͛̕а̶̧̪̽̓т̸̛̤͖́ь̶̺́͑ ̷̱͝д̶͖͋̊у̴̛̤ш̴̣̈́̌у̸͎͖̒ </button>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
   name: "GodsVi",
   el: '#gods',
+  mounted: function () {
+    // Attach event listener to the root vue element
+    this.$el.addEventListener('click', this.onClick)
+  },
+  beforeUnmount: function () {
+    this.$el.removeEventListener('click', this.onClick)
+  },
   data() {
     return {
-      message: 'Привет, Vue.js!'
+      message: 'Привет, Vue.js!',
+      addInfo: 'О великие боги, пощадите этого смертного Акакия, болеющего недугом собачья хворь',
+      nameP: 'Акакий Акакиевич Бронский',
+      job: 'плотник',
+      age: 42,
+      disease: 'собачья хворь',
+      clickCount:0,
+      cursed:false
     }
   },
   methods: {
-    reverseMessage: function () {
+    heal: function () {
       //java method
-      this.message = this.message.split('').reverse().join('')
+      this.clickCount=0
+      this.cursed=false
+    },
+    death:function () {
+      //other method
+      this.clickCount=0
+      this.cursed=false
+    },
+    onClick: function () {
+      this.clickCount+=1;
+      if (this.cursed === true){
+        this.cursed=false;
+      }
+      if (this.clickCount === 13){
+        this.clickCount=0;
+        this.cursed=true;
+      }
     }
   }
 }
