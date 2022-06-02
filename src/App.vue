5f4c70c98fedf44d659a081109052816fe61646a
annab <243845@niuitmo.ru>
2022-06-02 17:26:04 +0300

    not working

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Gods/>
+  <!--<AuthT/>-->
 </template>
 
 <script>
 import Gods from './components/Gods.vue'
+//import AuthT from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
     Gods
+    //AuthT
   }
 }
 </script>
--- src/components/Gods.vue
+++ src/components/Gods.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="gods">
     <div class="content">
-      <div> {{ dataInfo }}</div>
+
       <div><img class="cursed-image" src="../assets/cursed.png" alt="cursed" v-show="cursed"/></div>
       <table class="info">
         <tr>
         <td class="main-info">
           <p>Имя: {{ nameP }}</p>
           <p>Род деятельности: {{ job }}</p>
-          <p>Возраст: {{ age }}</p>
           <p>Болезнь: {{ disease }}</p>
         </td>
         </tr>
@@ -45,7 +44,6 @@ export default {
       addInfo: 'О великие боги, пощадите этого смертного Акакия, болеющего недугом собачья хворь',
       nameP: 'Акакий Акакиевич Бронский',
       job: 'плотник',
-      age: 42,
       disease: 'собачья хворь',
       clickCount:0,
       cursed:false,
@@ -57,13 +55,15 @@ export default {
       //java method
       this.clickCount=0
       this.cursed=false
-      this.getData('http://localhost:8080/api/v1/god/god/prayers/unanswered/last')
+      this.postData();
+      this.getData('https://localhost:8080/api/v1/god/god/prayers/unanswered/last')
     },
     death:function () {
       //other method
       this.clickCount=0
       this.cursed=false
-      this.getData('http://localhost:8080/api/v1/god/god/prayers/unanswered/last')
+      this.postData();
+      this.getData('https://localhost:8080/api/v1/god/god/prayers/unanswered/last')
     },
     onClick: function () {
       this.clickCount+=1;
@@ -75,16 +75,32 @@ export default {
         this.cursed=true;
       }
     },
-    getData: async function (url){
+    dealWithData (){
+      //Данные лежат в dataInfo и здесь парсятся куда надо
+      this.nameP=this.dataInfo.patient.name+this.dataInfo.patient.surname+this.dataInfo.patient.patronymic;
+      this.job=this.dataInfo.patient.socialStatus;
+      this.disease=this.dataInfo.diseaseName;
+      this.addInfo=this.dataInfo.text;
+    },
+    async postData(){
+      //TODO
+    },
+    async getData (url){
+      const headers = { "Content-Type": "application/json" };
       axios
-          .get(url, {},{
+          .get(url, {headers},{
             auth: {
               username: 'god',
               password: 'god_pass'
             }
           })
-          .then(response => {this.dataInfo = response})
-          .catch(error => {this.dataInfo = error})
+          .then(response => {
+            this.dataInfo = response;
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      this.dealWithData();
     }
 
   }
